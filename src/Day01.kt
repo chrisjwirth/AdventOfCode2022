33063e5d0f696ec5859abf30be6adbb4b9d28aa7
Chris Wirth <cwirth9@gmail.com>
2022-12-01 13:08:52 -0500

    Complete Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxWeight = 0
+        var currentWeight = 0
+
+        input.forEach {
+            if (it.isNotBlank()) {
+                currentWeight += it.toInt()
+            } else {
+                maxWeight = maxOf(maxWeight, currentWeight)
+                currentWeight = 0
+            }
+        }
+
+        return maxOf(maxWeight, currentWeight)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val allWeights = mutableListOf<Int>()
+        var currentWeight = 0
+
+        input.forEach {
+            if (it.isNotBlank()) {
+                currentWeight += it.toInt()
+            } else {
+                allWeights.add(currentWeight)
+                currentWeight = 0
+            }
+        }
+        if (currentWeight != 0) allWeights.add(currentWeight)
+
+        return allWeights.sortedDescending().subList(0, 3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
+    // Test
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
+    // Final
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
